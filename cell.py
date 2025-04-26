2c9b2a5de6c2de5b38c81827e8950983bac4b83e
kbpop <kp.petzold.bradley@gmail.com>
2025-04-26 16:23:06 -0400

    created init of files

---
+++ cell.py
@@ -0,0 +1,16 @@
+class Cell:
+    def __init__(self, value, row, col, screen):
+        self.value = value
+        self.row = row
+        self.col = col
+        self.screen = screen
+
+    def set_cell_value(self, value):
+        return None
+
+    def set_sketched_value(self, value):
+        return None
+
+    def draw(self):
+        
+        return None
---
+++ sudoku.py
@@ -0,0 +1,16 @@
+import pygame
+from cell import Cell
+
+pygame.init()
+WIDTH, HEIGHT = 1000, 1000
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('Sudoku')
+screen.fill((255,255,255))
+test = Cell(1,1,1,1)
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.quit:
+            pygame.quit()
+
+    pygame.display.update()
