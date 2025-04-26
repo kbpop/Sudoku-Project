83c34168922b276a7f7f07b8b4670c2ccce38102
kbpop <kp.petzold.bradley@gmail.com>
2025-04-26 17:07:44 -0400

    copied boilerplate from lecturer

--- cell.py
+++ cell.py
@@ -1,3 +1,4 @@
+import pygame
 class Cell:
     def __init__(self, value, row, col, screen):
         self.value = value
@@ -6,11 +7,31 @@ class Cell:
         self.screen = screen
 
     def set_cell_value(self, value):
-        return None
+        self.value = value
 
     def set_sketched_value(self, value):
         return None
 
     def draw(self):
-        
-        return None
+        font = pygame.font.Font(None,100)
+        cell_x_surf = font.render(self.value, 0, (100,200,0))
+        cell_x_rect = cell_x_surf.get_rect(center=(self.row*100,self.col*100))
+        self.screen.blit(cell_x_surf, cell_x_rect)
+
+if __name__ == '__main__':
+
+    pygame.init()
+    screen = pygame.display.set_mode()
+    test_cell = Cell("2",2,2,screen)
+    screen.fill((255,255,255))
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.quit:
+                pygame.quit()
+
+        pygame.display.update()
+        test_cell.draw()
+
+
+
--- sudoku.py
+++ sudoku.py
@@ -1,12 +1,12 @@
 import pygame
 from cell import Cell
 
+
 pygame.init()
 WIDTH, HEIGHT = 1000, 1000
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Sudoku')
 screen.fill((255,255,255))
-test = Cell(1,1,1,1)
 
 while True:
     for event in pygame.event.get():
